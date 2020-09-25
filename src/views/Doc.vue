62cc0f23774f2a08d79a5fb6e131fdf99e25bdae
guoxu1989 <guoxuguoxu1989@gmail.com>
2020-09-25 20:25:31 +0800

    基本完成官网

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <TopNav />
+  <div class="layout">
+    <TopNav class="nav"/>
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -41,10 +41,42 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0; 
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px;position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+
   > h2 {
     margin-bottom: 4px;
   }
@@ -59,5 +91,8 @@ aside {
     left: 0;
     padding-top: 70px;
   }
+}
+main {
+  overflow: auto;
 }
 </style>
