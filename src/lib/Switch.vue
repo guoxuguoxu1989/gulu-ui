5a07f837dd7d64d3cccf067ee6af2b79f006c2b8
guoxu1989 <guoxuguoxu1989@gmail.com>
2021-03-23 10:37:39 +0800

    动画完美了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button :class="{checked}" @click="toggle"><span></span></button>
   </div>
 </template>
+<script>
+import { ref } from 'vue'
+export default {
+  setup(){
+    const checked = ref(true)
+    const toggle = ()=>{
+      checked.value = !checked.value
+    }
+    return {
+      checked, toggle
+    }
+  }
+}
+</script>
 <style lang="scss">
   $h: 22px;
   $h2: $h - 4px;
@@ -10,7 +24,7 @@
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: grey;
     border-radius: $h/2;
     position: relative;
   }
@@ -22,5 +36,13 @@
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
-  } 
+    transition: left 250ms;
+  }
+  button.checked{
+    background: blue;
+  }
+  button.checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+  button:focus{ outline: none;}
 </style>
